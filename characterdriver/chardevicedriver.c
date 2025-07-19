be3d21ed1bfa4972bac5ad30bd81beb0c264ccfe
shivateja769 <shivateja769@gmail.com>
2025-07-19 18:09:35 +0530

    implemented ioctl to send data to kernel and receive from kernel

--- characterdriver/chardevicedriver.c
+++ characterdriver/chardevicedriver.c
@@ -4,21 +4,18 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/ioctl.h>
-#include <linux/string.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Shiva Teja");
-MODULE_DESCRIPTION("Simple Char Device Driver with IOCTL");
+MODULE_DESCRIPTION("Char Driver with IOCTL WR_DATA and RD_DATA");
 
 #define DEVICE_NAME "simple_char_driver"
-
 #define MAGIC_NUM 100
-#define IOCTL_PRINT       _IO(MAGIC_NUM, 0)
-#define IOCTL_GET_VALUE   _IOR(MAGIC_NUM, 1, int *)
-#define IOCTL_SET_VALUE   _IOW(MAGIC_NUM, 2, int)
+#define WR_DATA _IOW(MAGIC_NUM, 1, int)
+#define RD_DATA _IOR(MAGIC_NUM, 2, int)
 
 static int major_number;
-static int kernel_value = 0;
+static int kernel_val = 0;
 
 int chardev_open(struct inode *, struct file *);
 ssize_t chardev_read(struct file *, char __user *, size_t, loff_t *);
@@ -36,53 +33,37 @@ static struct file_operations my_file_operations = {
 };
 
 int chardev_open(struct inode *pinode, struct file *pfile) {
-    printk(KERN_INFO "simple_char_driver: Open called\n");
+    printk(KERN_INFO "Driver: Open\n");
     return 0;
 }
 
 ssize_t chardev_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) {
-    const char *message = "Hello from Kernel!\n";
-    size_t message_len = strlen(message);
-
-    printk(KERN_INFO "simple_char_driver: Read called\n");
-
-    if (*offset >= message_len)
-        return 0;
-
-    if (copy_to_user(buffer, message, message_len))
-        return -EFAULT;
-
-    *offset += message_len;
-    return message_len;
+    printk(KERN_INFO "Driver: Read called\n");
+    return 0;
 }
 
 ssize_t chardev_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) {
-    printk(KERN_INFO "simple_char_driver: Write called\n");
+    printk(KERN_INFO "Driver: Write called\n");
     return length;
 }
 
 int chardev_release(struct inode *pinode, struct file *pfile) {
-    printk(KERN_INFO "simple_char_driver: Release called\n");
+    printk(KERN_INFO "Driver: Release\n");
     return 0;
 }
 
 long chardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     switch (cmd) {
-        case IOCTL_PRINT:
-            printk(KERN_INFO "IOCTL_PRINT called\n");
+        case WR_DATA:
+            if (copy_from_user(&kernel_val, (int *)arg, sizeof(kernel_val)))
+                return -EFAULT;
+            printk(KERN_INFO "IOCTL WR_DATA: Received value = %d\n", kernel_val);
             break;
 
-        case IOCTL_GET_VALUE:
-            printk(KERN_INFO "IOCTL_GET_VALUE called, returning %d\n", kernel_value);
-            if (copy_to_user((int *)arg, &kernel_value, sizeof(kernel_value)))
+        case RD_DATA:
+            if (copy_to_user((int *)arg, &kernel_val, sizeof(kernel_val)))
                 return -EFAULT;
-            break;
-
-        case IOCTL_SET_VALUE:
-            printk(KERN_INFO "IOCTL_SET_VALUE called\n");
-            if (copy_from_user(&kernel_value, (int *)arg, sizeof(kernel_value)))
-                return -EFAULT;
-            printk(KERN_INFO "New kernel_value: %d\n", kernel_value);
+            printk(KERN_INFO "IOCTL RD_DATA: Sent value = %d\n", kernel_val);
             break;
 
         default:
@@ -92,21 +73,21 @@ long chardev_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
 }
 
 static int __init ldd_init(void) {
-    printk(KERN_INFO "simple_char_driver: Initializing...\n");
+    printk(KERN_INFO "Driver: Initializing...\n");
 
     major_number = register_chrdev(0, DEVICE_NAME, &my_file_operations);
     if (major_number < 0) {
-        printk(KERN_ALERT "Failed to register device\n");
+        printk(KERN_ALERT "Driver: Register failed\n");
         return major_number;
     }
 
-    printk(KERN_INFO "Registered device with major number %d\n", major_number);
+    printk(KERN_INFO "Driver: Registered with major number %d\n", major_number);
     return 0;
 }
 
 static void __exit ldd_exit(void) {
     unregister_chrdev(major_number, DEVICE_NAME);
-    printk(KERN_INFO "simple_char_driver: Unregistered device and exiting\n");
+    printk(KERN_INFO "Driver: Unregistered\n");
 }
 
 module_init(ldd_init);
--- characterdriver/userprog.c
+++ characterdriver/userprog.c
@@ -1,62 +1,39 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/ioctl.h>
 
 #define DEVICE_PATH "/dev/simple_char_driver"
-
 #define MAGIC_NUM 100
-#define IOCTL_PRINT       _IO(MAGIC_NUM, 0)
-#define IOCTL_GET_VALUE   _IOR(MAGIC_NUM, 1, int *)
-#define IOCTL_SET_VALUE   _IOW(MAGIC_NUM, 2, int)
+#define WR_DATA _IOW(MAGIC_NUM, 1, int)
+#define RD_DATA _IOR(MAGIC_NUM, 2, int)
 
 int main() {
-    int fd;
-    char write_buf[] = "Hello from Userspace!";
-    char read_buf[100];
-    int ioctl_value = 42;
+    int fd, value;
 
     fd = open(DEVICE_PATH, O_RDWR);
     if (fd < 0) {
-        perror("Failed to open the device");
+        perror("Failed to open device");
         return 1;
     }
 
-    // Write to device
-    if (write(fd, write_buf, strlen(write_buf)) < 0) {
-        perror("Failed to write to the device");
-        close(fd);
-        return 1;
-    }
-    printf("Written to device: %s\n", write_buf);
+    // Take input from console
+    printf("Enter an integer to send to the kernel: ");
+    scanf("%d", &value);
 
-    // Read from device
-    if (read(fd, read_buf, sizeof(read_buf)) < 0) {
-        perror("Failed to read from the device");
-        close(fd);
-        return 1;
-    }
-    printf("Read from device: %s\n", read_buf);
-
-    // IOCTL SET
-    if (ioctl(fd, IOCTL_SET_VALUE, &ioctl_value) < 0) {
-        perror("IOCTL_SET_VALUE failed");
+    // Send value using ioctl WR_DATA
+    if (ioctl(fd, WR_DATA, &value) < 0) {
+        perror("WR_DATA ioctl failed");
     } else {
-        printf("Sent ioctl set value: %d\n", ioctl_value);
+        printf("Sent to kernel: %d\n", value);
     }
 
-    // IOCTL GET
-    ioctl_value = 0;
-    if (ioctl(fd, IOCTL_GET_VALUE, &ioctl_value) < 0) {
-        perror("IOCTL_GET_VALUE failed");
+    // Read back using ioctl RD_DATA
+    value = 0;
+    if (ioctl(fd, RD_DATA, &value) < 0) {
+        perror("RD_DATA ioctl failed");
     } else {
-        printf("Received ioctl get value: %d\n", ioctl_value);
-    }
-
-    // IOCTL PRINT
-    if (ioctl(fd, IOCTL_PRINT) < 0) {
-        perror("IOCTL_PRINT failed");
+        printf("Received from kernel: %d\n", value);
     }
 
     close(fd);
